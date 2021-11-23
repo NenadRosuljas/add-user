c1901e20b2699a98801c7e1b28fcb07563de4b78
Nenad Rosuljas <n.rosuljas@vega.local>
2021-11-23 11:40:13 +0100

    Added styles for UsersList component from CSS module.

--- src/components/Users/UsersList.jsx
+++ src/components/Users/UsersList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Card from '../UI/Card';
+import classes from './UsersList.module.css';
 
 const UsersList = (props) => {
   return (
-    <Card>
+    <Card className={classes.users}>
       <ul>
         {props.users.map((user) => {
           <li>
