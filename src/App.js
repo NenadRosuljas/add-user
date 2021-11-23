f1c9b7a2b8bfefbc4d2661fae26c7f147ab0543d
Nenad Rosuljas <n.rosuljas@vega.local>
2021-11-23 11:34:17 +0100

    Added UsersList component for displaying users below the form.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 import './App.css';
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UsersList users={[]} />
     </div>
   );
 }
