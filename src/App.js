a206e03c74bf3b0113a50fa254000ff52672a1ca
Nenad Rosuljas <n.rosuljas@vega.local>
2021-11-23 13:23:32 +0100

    Added function for displaying users that are submitted.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
+import React, { useState } from 'react';
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
 import './App.css';
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (userName, userAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { name: userName, age: userAge, id: Math.random().toString() },
+      ];
+    });
+  };
+
   return (
     <div>
-      <AddUser />
-      <UsersList users={[]} />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -16,6 +16,8 @@ const AddUser = (props) => {
       return;
     }
 
+    props.onAddUser(enteredUsername, enteredAge);
+
     setEnteredUsername('');
     setEnteredAge('');
   };
